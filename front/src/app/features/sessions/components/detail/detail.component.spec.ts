5759a1dd09ab73ae7f2ac83ef1cdc34d86107da3
Meceline <celinemercadier@gmail.com>
2024-05-02 11:26:29 +0200

    FRONT : test form.component

--- front/src/app/features/sessions/components/detail/detail.component.spec.ts
+++ front/src/app/features/sessions/components/detail/detail.component.spec.ts
@@ -26,7 +26,7 @@ describe('DetailComponent', () => {
   }
   const mockSession: Session = {
     id: 1,
-    name: 'Test Session',
+    name: 'Test Session !!',
     description: "desc",
     date: new Date(),
     teacher_id: 1,
@@ -48,37 +48,22 @@ describe('DetailComponent', () => {
       providers: [{ provide: SessionService, useValue: mockSessionService }],
     })
       .compileComponents();
+      sessionApiService = TestBed.inject(SessionApiService);
       service = TestBed.inject(SessionService);
-    fixture = TestBed.createComponent(DetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      fixture = TestBed.createComponent(DetailComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should fetch and display session details on init', () => {
-    const id = "1";
-    component.sessionId = id;
-    
-    const detailSpy = jest.spyOn(sessionApiService, 'detail').mockReturnValue(of({} as Session))
-
-    component.ngOnInit();
-
-    fixture.detectChanges();
-    expect(component.userId).toEqual(mockSessionService.sessionInformation.id.toString());
-
-    expect(detailSpy).toHaveBeenCalledWith(id);
-    expect(component.session).toEqual(mockSession);
-  });
-
+  
   it('should navigate back when back button is cliked', () => {
     const backSpy = jest.spyOn(window.history, 'back');
     component.back();
     expect(backSpy).toHaveBeenCalled();
   });
-
 
   it('should allow an admin to delete a session', () => {
     mockSessionService.sessionInformation.admin = true;
@@ -91,13 +76,52 @@ describe('DetailComponent', () => {
     component.delete();
     expect(deleteSpy).toHaveBeenCalledWith(component.sessionId);
   });
+
+  it('should call participate method of sessionApiService and fetchSession', () => {
+    component.session = mockSession; // Étant donné une session avec des utilisateurs existants
+    const participateSpy = jest.spyOn(sessionApiService, 'participate').mockReturnValue(of(undefined)); // Espionner la méthode participate du service
+    const detailSpy  = jest.spyOn(sessionApiService, 'detail').mockReturnValue(of({...mockSession, users: [1]})); // Espionner la méthode detail du service et la remplacer par une observable retournant une session modifiée
+
+    expect(component.userId).toEqual(mockSessionService.sessionInformation.id.toString()); // Vérifier que l'identifiant de l'utilisateur est correctement défini
+
+    component.participate();  // Quand l'utilisateur participe à la session
+    expect(participateSpy).toHaveBeenCalledWith(component.sessionId, component.userId);// Vérifier que la méthode participate a été appelée avec les bons paramètres
+    expect(detailSpy).toHaveBeenCalledWith(component.sessionId); // Vérifier que la méthode detail a été appelée avec l'identifiant de session
+    expect(component.session.users).toContain(1);  // Vérifier que l'utilisateur a été ajouté à la liste des utilisateurs de la session
+    expect(component.isParticipate).toBe(true); // Vérifier que la variable isParticipate est définie à true
+  });
+
+  it('should call unparticipate method of sessionApiService and fetchSession when unParticipate is called', () => {
+    expect(component.userId).toEqual(mockSessionService.sessionInformation.id.toString());
+
+    component.session = mockSession;
+    const unparticipateSpy = jest.spyOn(sessionApiService, 'unParticipate').mockReturnValue(of(undefined));
+    const detailSpy = jest.spyOn(sessionApiService, 'detail').mockReturnValue(of({...mockSession, users: []}));
+
+    component.unParticipate();
+
+    expect(unparticipateSpy).toHaveBeenCalledWith(component.sessionId, component.userId);
+    expect(detailSpy).toHaveBeenCalledWith(component.sessionId);
+    expect(component.session.users).not.toContain(1);
+    expect(component.isParticipate).toBe(false);
+  });
+  
+  it('should fetch and display session details on init', () => {
+    const id = "1";
+    component.sessionId = id;
+    const detailSpy = jest.spyOn(sessionApiService, 'detail').mockReturnValue(of(mockSession));
+
+    component.ngOnInit();
+
+    fixture.detectChanges();
+    expect(component.userId).toEqual(mockSessionService.sessionInformation.id.toString());
+
+    expect(detailSpy).toHaveBeenCalledWith(id);
+    expect(component.session).toEqual(mockSession);
+  });
+
+
+
 });
 
-/*
- Cannot spyOn on a primitive value; undefined given
-
-      88 |
-      89 |     expect(component.isAdmin).toBe(true);
-    > 90 |     const deleteSpy = jest.spyOn(sessionApiService, 'delete').mockReturnValue(of(null));
-*/
 
--- front/src/app/features/sessions/components/form/form.component.spec.ts
+++ front/src/app/features/sessions/components/form/form.component.spec.ts
@@ -1,12 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {  ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
@@ -14,16 +14,30 @@ import { SessionService } from 'src/app/services/session.service';
 import { SessionApiService } from '../../services/session-api.service';
 
 import { FormComponent } from './form.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Session } from '../../interfaces/session.interface';
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { spyOn } from 'jest-mock';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
-
+  let route: ActivatedRoute;
+  let router : Router;
+  let httpMock: HttpTestingController;
+  let matSnackBar: MatSnackBar;
+  
   const mockSessionService = {
     sessionInformation: {
       admin: true
     }
   } 
+  const mockSession: Partial<Session> = {
+    name: 'testSession',
+    description: 'test session description',
+    date: new Date(),
+    teacher_id: 1
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -38,11 +52,18 @@ describe('FormComponent', () => {
         ReactiveFormsModule, 
         MatSnackBarModule,
         MatSelectModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
       ],
       providers: [
         { provide: SessionService, useValue: mockSessionService },
-        SessionApiService
+        SessionApiService,
+        {
+          provide: MatSnackBar,
+          useValue: {
+            open: jest.fn()
+          }
+        }
       ],
       declarations: [FormComponent]
     })
@@ -50,10 +71,53 @@ describe('FormComponent', () => {
 
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    route = TestBed.inject(ActivatedRoute);
+    router = TestBed.inject(Router);
+    httpMock = TestBed.inject(HttpTestingController);
+    matSnackBar = TestBed.inject(MatSnackBar);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create form with empty fields when the route is create', () => {
+    component.ngOnInit();
+    expect(component.sessionForm).toBeTruthy();
+    expect(component.sessionForm?.dirty).toBeFalsy();
+    expect(component.sessionForm?.contains('name')).toBeTruthy();
+    expect(component.sessionForm?.contains('date')).toBeTruthy();
+    expect(component.sessionForm?.contains('teacher_id')).toBeTruthy();
+    expect(component.sessionForm?.contains('description')).toBeTruthy();
+  });
+
+  it('should initialized form with session details when rout is update', () => { 
+    const id='1';
+    jest.spyOn(route.snapshot.paramMap, 'get').mockReturnValue(id);
+    jest.spyOn(router, 'url', 'get').mockReturnValue('update');
+    component.ngOnInit();
+    expect(component.sessionForm).toBeDefined();
+    expect(component.onUpdate).toBeTruthy();
+    
+    const req = httpMock.expectOne(`api/session/${id}`);
+    expect(req.request.method).toBe('GET');
+
+    const formateDate = mockSession.date?.toISOString().split('T')[0];
+    const mockSessionWithDate = {...mockSession, date: formateDate};
+
+    req.flush(mockSessionWithDate);
+    expect(component.sessionForm?.value).toEqual(mockSessionWithDate);
+  });
+  
+  it('should navigate to "sessions" route after showing message', () => {
+    const message = 'Test message';
+    const matSnackBarSpy = spyOn(TestBed.inject(MatSnackBar), 'open');
+    const routerSpy = spyOn(TestBed.inject(Router), 'navigate');
+  
+    component['exitPage'](message);
+  
+    expect(matSnackBarSpy).toHaveBeenCalledWith(message, 'Close', { duration: 3000 });
+    expect(routerSpy).toHaveBeenCalledWith(['sessions']);
   });
 });
